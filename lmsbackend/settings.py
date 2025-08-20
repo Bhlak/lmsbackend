115811306c457e962d6511d74e283d0250d234f6
Adekoya Ibukunoluwa <100376947+Bhlak@users.noreply.github.com>
2025-08-20 07:42:59 +0100

    Cors fix attempt

--- lmsbackend/settings.py
+++ lmsbackend/settings.py
@@ -66,7 +66,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-CORS_ORIGIN_WHITELIST = ("https://curious-shortbread-00dad2.netlify.app/")
+# CORS_ORIGIN_WHITELIST = ("https://curious-shortbread-00dad2.netlify.app/")
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'lmsbackend.urls'
 
